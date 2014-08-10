d702866db35ac541506c3043e17d9bfbb70d8d3e
JJtCool <josh.thompson121@gmail.com>
2014-08-10 19:31:55 +0100

    Second commit - UHC Lobby

--- src/me/jjtcool/uhc/lobby/JoinEvent.java
+++ src/me/jjtcool/uhc/lobby/JoinEvent.java
@@ -9,6 +9,8 @@ package me.jjtcool.uhc.lobby;
  */
 
 import ca.wacos.nametagedit.NametagAPI;
+import com.dsh105.holoapi.api.Hologram;
+import com.dsh105.holoapi.api.HologramFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -21,6 +23,7 @@ import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.*;
+import org.bukkit.util.Vector;
 
 /**
  * Created with IntelliJ IDEA.
@@ -55,6 +58,28 @@ public class JoinEvent extends JavaPlugin implements Listener {
 
     private static Team owner;
 
+    public static com.dsh105.holoapi.api.HoloManager getHoloManager() {
+        return com.dsh105.holoapi.HoloAPI.getManager();
+    }
+
+    Hologram hologram = new HologramFactory(this)    // Replace "myPlugin" with your plugin instance
+            .withLocation(new Vector(1, 1, 1), "uhclobby")
+            .withText(ChatColor.GRAY + "====================")
+            .withText(ChatColor.BOLD + "" + ChatColor.GREEN + "Welcome to the MCSquared Network " + ChatColor.BOLD + "" + ChatColor.RED + "UHC")
+            .withText("")
+            .withText(ChatColor.BOLD + "" + ChatColor.AQUA + "Enter the lobby by going through the portal!")
+            .withText(ChatColor.RED + "<" + ChatColor.GRAY + "--------" + ChatColor.RED + ">")
+            .withText("")
+            .withText(ChatColor.BLUE + "Welcome to a network, with community at it's core. Here at MCSquared we strive to have the best " + ChatColor.BLUE +
+                    "community as we feel that's the heart of a good server, along with the infrastructure needed to run the gamemodes players love.")
+            .withText("")
+            .withText(ChatColor.YELLOW + "This is a pre-release for the MCSquared Network, ")
+            .withText(ChatColor.YELLOW + "we are working as hard as we can to get everything ready!")
+            .withText(ChatColor.LIGHT_PURPLE + "Check out our website - " + ChatColor.BOLD + "" + ChatColor.GOLD + "www.mc-sq.net")
+            .withText(ChatColor.AQUA + "Check out the Developers Blog - www.mc-sq.net/developersblog.html")
+            .withText(ChatColor.GRAY + "====================")
+            .build();
+
 
     public void createScoreboard(final Player player) {
         board = Bukkit.getServer().getScoreboardManager().getNewScoreboard();
@@ -72,7 +97,7 @@ public class JoinEvent extends JavaPlugin implements Listener {
 
         final Score score3 = obj.getScore(Bukkit.getOfflinePlayer(ChatColor.BOLD + "" + ChatColor.AQUA + "Online: "));
 
-        final Score scoreOnline = obj.getScore(Bukkit.getOfflinePlayer(ChatColor.AQUA + "" + Bukkit.getOnlinePlayers() + " Players online"));
+        final Score scoreOnline = obj.getScore(Bukkit.getOfflinePlayer(ChatColor.AQUA + "" + Bukkit.getOnlinePlayers() + " Players Online"));
 
         final Score score4 = obj.getScore(Bukkit.getOfflinePlayer(ChatColor.BOLD + "" + ChatColor.GREEN + "Server: "));
 
@@ -91,6 +116,7 @@ public class JoinEvent extends JavaPlugin implements Listener {
                         String next = scroller.next();
                         obj.setDisplayName(next);
                         player.setScoreboard(board);
+                        Score scoreOnline = obj.getScore(Bukkit.getOfflinePlayer(ChatColor.AQUA + "" + Bukkit.getOnlinePlayers() + "Players Online"));
                     }
                 } catch (Exception e) {
                     this.cancel();
